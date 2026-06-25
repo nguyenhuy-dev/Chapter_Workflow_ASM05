ed604c75f2f512fa59b7bb8190585d0bacdbe0b9
nguyenhuy-dev <nguyenhuy.se205@gmail.com>
2026-06-25 19:57:36 +0700

    init project

---
+++ MangaWorkflow.ChapterMetas.Microservice.HuyNQ/Controllers/ChapterMetasController.cs
@@ -0,0 +1,58 @@
+﻿using MangaWorkflow.BusinessObject.Shared.Models.HuyNQ;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MangaWorkflow.Chapters.Microservice.HuyNQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChapterMetasController : ControllerBase
+    {
+        private List<ChapterMetaHuyNq> _metas = [];
+
+        public ChapterMetasController()
+        {
+            _metas =
+            [
+                new() {
+                    HuynqId = 1,
+                    Code = "101",
+                    Description = "Vuyp"
+                }
+            ];
+        }
+
+        // GET: api/<ChapterMetasController>
+        [HttpGet]
+        public IEnumerable<ChapterMetaHuyNq> Get()
+        {
+            return _metas;
+        }
+
+        // GET api/<ChapterMetasController>/5
+        [HttpGet("{id}")]
+        public ChapterMetaHuyNq Get(int id)
+        {
+            return _metas.Find(c => c.HuynqId == id) ?? new();
+        }
+
+        // POST api/<ChapterMetasController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ChapterMetasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ChapterMetasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MangaWorkflow.Chapters.Microservice.HuyNQ/Controllers/ChaptersController.cs
@@ -0,0 +1,63 @@
+﻿using MangaWorkflow.BusinessObject.Shared.Models.HuyNQ;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MangaWorkflow.Chapters.Microservice.HuyNQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChaptersController : ControllerBase
+    {
+        private List<ChapterHuyNq> _chapters = [];
+
+        public ChaptersController()
+        {
+            _chapters =
+            [
+                new()
+                {
+                    HuynqId = 1,
+                    ChapterMetaHuynqId = 1,
+                    Title = "Son Goku",
+                    ChapterNumber = 3,
+
+                }
+            ];
+        }
+
+        // GET: api/<ChaptersController>
+        [HttpGet]
+        public IEnumerable<ChapterHuyNq> Get()
+        {
+            return _chapters;
+        }
+
+        // GET api/<ChaptersController>/5
+        [HttpGet("{id}")]
+        public ChapterHuyNq Get(int id)
+        {
+            return _chapters.Find(c => c.HuynqId == id) ?? new();
+        }
+
+        // POST api/<ChaptersController>
+        [HttpPost]
+        public void Post([FromBody] ChapterHuyNq value)
+        {
+            Console.WriteLine("Submit order: " + JsonSerializer.Serialize(value));
+        }
+
+        // PUT api/<ChaptersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ChaptersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
